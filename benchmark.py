d24eadce7522e14db9e70031dfa8e6be57f9756e
Vincent Ying <vincenthying@gmail.com>
2017-12-07 20:06:54 -0800

    Starting write of graph-tool graph for eigentrust.

--- benchmark.py
+++ benchmark.py
@@ -6,6 +6,7 @@
 ################################################################################
 
 
+import graph_tool.all as gt
 import networkx as nx
 import tidaltrust as tt
 
@@ -19,4 +20,22 @@ fh.close()
 
 
 # Tidaltrust of single edge.
-print tt.compute_trust(btcAlphaGr, 1, 888)["trust"]
+u = 1
+v = 888
+if (btcAlphaGr.has_edge(u, v)):
+  print tt.compute_trust(btcAlphaGr, 1, 888)["trust"]
+
+
+# Eigentrust
+# Trust values must lie between [0,1]
+
+g = gt.Graph()
+# Add vertices
+for node in btcAlphaGr.nodes():
+  g.add_vertex(node)
+# Add edges
+for u, v in btcAlphaGr.edges():
+  g.add_edge(u, v)
+# Add edge weight???????
+
+#gt.centrality.eigentrust(g, trust_map, vprop=None, norm=False, epsilon=1e-06, max_iter=0, ret_iter=False)
